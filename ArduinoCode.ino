16190868d1ed4f34d295a0dc4dea94ded2c1be7a
TsuDaved <davidstevekong@gmail.com>
2026-04-28 13:25:31 +0100

    Add pin definitions and empty setup/loop

---
+++ ArduinoCode.ino
@@ -0,0 +1,16 @@
+// ===== PIN DEFINITIONS =====
+const int leftSensor  = A0;
+const int rightSensor = A1;
+const int leftMotorPWM  = 10;
+const int rightMotorPWM = 11;
+
+void setup()
+{
+  pinMode(leftMotorPWM, OUTPUT);
+  pinMode(rightMotorPWM, OUTPUT);
+}
+
+void loop()
+{
+  // to be implemented
+}
